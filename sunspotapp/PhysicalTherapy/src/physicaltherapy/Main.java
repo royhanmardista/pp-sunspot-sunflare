83ae1dba5bf13e6a70fd843deee251cca176184f
wyip <wyip@ff245518-688a-11de-a9b1-77b8bb241e6b>
2008-04-30 04:35:23 +0000

    sunspotapp added

---
+++ sunspotapp/PhysicalTherapy/src/physicaltherapy/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Main.java
+ *
+ * Created on April 29, 2008, 1:37 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package physicaltherapy;
+import physicaltherapy.gui.GUIFrame;
+/**
+ *
+ * @author Winnie
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUIFrame frame = new GUIFrame();
+    }
+    
+}
